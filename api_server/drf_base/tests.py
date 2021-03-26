a0357bfda811c5698e7c122dc51494f19fdaf316
Pablo Centurión <pablushka@nomades.com.ar>
2021-03-26 02:12:03 -0300

    Add the first test

--- api_server/drf_base/tests.py
+++ api_server/drf_base/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.contrib.auth.models import User
+import json
 
-# Create your tests here.
+class RegistrationTestCase(APITestCase):
+
+    def test_api_availability(self):
+        response = self.client.get('/api/')
+        self.assertEqual(response.status_code, status.HTTP_200_CREATED)
